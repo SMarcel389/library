410ebb4962a42303bf8e55a6235272d03088743c
SMarcel389 <158337939+SMarcel389@users.noreply.github.com>
2025-11-09 01:42:25 +0100

    v1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# library
+Photo by <a href="https://unsplash.com/@thomasbormans?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Thomas Bormans</a> on <a href="https://unsplash.com/photos/books-on-bookshelf-near-ladder-IHQHXj3jv6E?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+      # library
---
+++ script.js
@@ -0,0 +1,71 @@
+const shelf = document.querySelector(".shelf")
+
+const myLibrary = [];
+myLibrary.push( new Book("Harry Potter", "JK Rowling", 320, false) )
+myLibrary.push( new Book("The Hobbit", "JRR Tolkien", 420, false) )
+
+function Book(title, author, pages, read, thumbnail) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read
+  this.thumbnail = thumbnail
+  this.id = crypto.randomUUID(title)
+}
+
+
+
+const form = document.querySelector("#book-add")
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const formResult = new FormData(form);
+    const bookData = Object.fromEntries(formResult.entries())
+
+    if (bookData.title !=="") {
+        myLibrary.push( new Book(bookData.title, bookData.author, bookData.pages, bookData.read, bookData.thumbnail))
+        displayBooks()
+    }
+    else {
+        alert("Your book must have a title!")
+    }
+})
+
+function displayBooks() {
+    shelf.innerHTML = ""
+    shelf.classList.add("filled")
+    for (const Book of myLibrary) {
+        const bookCard = document.createElement("div")
+        bookCard.className = "bookCard"
+
+        if (Book.thumbnail) {
+            bookCard.classList.add("bookWithImage")
+            bookCard.style.backgroundImage = `url(${Book.thumbnail})`
+            console.log(Book.thumbnail)
+        }
+        
+        
+        const bookTitle = document.createElement("h4");
+        bookTitle.textContent = Book.title
+
+        const bookAuthor = document.createElement("p");
+        bookAuthor.textContent = Book.author;
+
+        const bookPages = document.createElement("p");
+        if (Book.pages) {
+        bookPages.textContent = "Pages: " + Book.pages
+        }
+        
+        const bookRead = document.createElement("div")
+        if (Book.read) {
+            bookRead.className = "bookIsRead"
+        }
+        else {
+            bookRead.className = "bookIsNotRead"
+        }
+        
+        bookCard.append(bookTitle, bookAuthor, bookPages, bookRead)
+        shelf.append(bookCard)
+        }
+    }
+
+
